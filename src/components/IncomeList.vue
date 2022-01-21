b32666bb9c3371190fff1c72bee9d31af9cd2da3
Alvin Galit <aroblesgalit@gmail.com>
2022-01-21 00:10:12 -0700

    Imported IncomeItem component into IncomeList

--- src/components/IncomeList.vue
+++ src/components/IncomeList.vue
@@ -1,11 +1,21 @@
 <template>
   <div class="income-list">
-    <!-- ... -->
+    <IncomeItem />
   </div>
 </template>
 
 <script>
-export default {}
+import IncomeItem from './IncomeItem'
+
+export default {
+  name: 'IncomeList',
+  components: {
+    IncomeItem
+  },
+  props: {
+    state: Object
+  }
+}
 </script>
 
 <style scoped>
